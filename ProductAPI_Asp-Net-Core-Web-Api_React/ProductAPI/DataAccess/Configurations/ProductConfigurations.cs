934d4877fecfa2dae4c225886d755a5490fc8b60
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2024-10-15 18:36:17 +0300

    Update Price Decimal Limit

--- ProductAPI_Asp-Net-Core-Web-Api_React/ProductAPI/DataAccess/Configurations/ProductConfigurations.cs
+++ ProductAPI_Asp-Net-Core-Web-Api_React/ProductAPI/DataAccess/Configurations/ProductConfigurations.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,9 @@ namespace DataAccess.Configurations
             builder.Property(p => p.Name)
                 .IsRequired()
                 .HasMaxLength(100);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,2");
 
             builder.HasOne(p => p.Category)
                 .WithMany(c => c.Products)
